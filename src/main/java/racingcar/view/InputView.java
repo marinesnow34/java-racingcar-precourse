4c1928fd7ed436807f345151693c0651a6a465b5
marinesnow34 <m4rinesnow34@gmail.com>
2022-12-04 12:15:36 +0900

    refactor: 이름 입력 리스트로 쪼개기

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -2,9 +2,15 @@ package racingcar.view;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class InputView {
-    public String inputName() {
-        return readLine();
+    private static final String MESSAGE_NAME = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+
+    public List<String> inputNames() {
+        System.out.println(MESSAGE_NAME);
+        return Arrays.asList(readLine().split(","));
     }
 
     public String inputCount() {
