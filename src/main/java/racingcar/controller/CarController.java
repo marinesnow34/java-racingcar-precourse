5a4b3a536663dafedfcc9af041170b1f7a5862fa
marinesnow34 <m4rinesnow34@gmail.com>
2022-12-04 12:38:22 +0900

    feat: controller 유저에 따른 자동차 생성

--- src/main/java/racingcar/controller/CarController.java
+++ src/main/java/racingcar/controller/CarController.java
@@ -1,5 +1,27 @@
 package racingcar.controller;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import racingcar.Car;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
 public class CarController {
-    
+    private final InputView inputView;
+    private final OutputView outputView;
+    private List<Car> cars;
+
+    public CarController(OutputView outputView) {
+        this.inputView = new InputView();
+        this.outputView = new OutputView();
+    }
+
+    public void start() {
+        inputUser();
+    }
+
+    private void inputUser() {
+        List<String> users = inputView.inputNames();
+        cars = users.stream().map(Car::new).collect(Collectors.toList());
+    }
 }
